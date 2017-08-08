d6c77150f1b801902dd18964e0cbb5c3d4d382c5
Ammar Naqvi <13besesabbas@seecs.edu.pk>
2017-08-08 19:09:05 +0500

    small qol changes

--- registration/views.py
+++ registration/views.py
@@ -11,32 +11,39 @@ from django.views import View
 
 from .forms import ProfileForm, UserForm
 
+
 class SignUpView(View):
+
     def get(self, request):
-	    return render(request, 'registration/signup.html')
+        return render(request, 'registration/signup.html')
 
     def post(self, request):
-	    user = User.objects.create_user(request.POST['username'], request.POST[
-	                                    'email'], request.POST['password'])
-	    user.save()
-	    return render(request, 'registration/login.html')
+        user = User.objects.create_user(request.POST['username'], request.POST[
+                                        'email'], request.POST['password'])
+        user.save()
+        return render(request, 'registration/login.html')
+
 
 class HomeView(View):
+
     def get(self, request):
-	    return render(request, 'registration/index.html')
+        return render(request, 'registration/index.html')
+
 
 class ProfileView(View):
-	def get(self, request):
-		user_form = UserForm(instance=request.user)
-		profile_form = ProfileForm(instance=request.user.profile)
-		return render(request, 'registration/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
-	def post(self, request):
-		user_form = ProfileForm(request.POST, instance=request.user)
-		profile_form = ProfileForm(request.POST, instance=request.user.profile)
-		if all((user_form.is_valid(), profile_form.is_valid())):
-			user = user_form.save()
-			profile = profile_form.save(commit=False)
-			profile.user = request.user
-			profile.save()
-			return redirect('home')
+    def get(self, request):
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+        return render(request, 'registration/profile.html', {
+        	'user_form': user_form,
+        	'profile_form': profile_form
+        })
+
+    def post(self, request):
+        user_form = ProfileForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('home')
