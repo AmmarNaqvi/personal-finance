2e5c9ad10c3a453c4be25d91db3f7be20561aef9
Ammar Naqvi <13besesabbas@seecs.edu.pk>
2017-08-18 16:40:39 +0500

    category and transaction models

--- registration/admin.py
+++ registration/admin.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Category
+from .models import Profile, IncomeCategory, ExpenditureCategory, IncomeTransaction, ExpenditureTransaction
 
 admin.site.register(Profile)
-admin.site.register(Category)
+admin.site.register(IncomeCategory)
+admin.site.register(ExpenditureCategory)
+admin.site.register(IncomeTransaction)
+admin.site.register(ExpenditureTransaction)
